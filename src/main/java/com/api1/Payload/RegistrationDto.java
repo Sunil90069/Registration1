348111fc52a118d90730beaf335b45b802157f01
sunil <sk367114@gamil.com>
2024-10-01 15:04:24 +0530

    modified Dto

---
+++ src/main/java/com/api1/Payload/RegistrationDto.java
@@ -0,0 +1,12 @@
+package com.api1.Payload;
+
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+public class RegistrationDto {
+    private String name;
+    private String email;
+    private String mobile;
+
+}
